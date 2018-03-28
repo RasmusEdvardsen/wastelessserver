22109ac649e70a5acd97ab208dfadb583cc691ee
Rasmus Edvardsen <edvardsen.rasmus@gmail.com>
2018-03-28 12:53:10 +0200

    working connection to database

--- wasteless/Controllers/HomeController.cs
+++ wasteless/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +10,12 @@ namespace wasteless.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly string connString = ConfigurationManager.ConnectionStrings["wastelessDB"].ConnectionString;
         // GET: Login
         public ActionResult Home()
         {
+            //SqlConnection conn = new SqlConnection(connString);
+            //conn.Open();
             return View();
         }
     }
