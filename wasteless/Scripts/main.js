84c9a37da5777b833b5056c2cd082f7e93ea544b
Rasmus Edvardsen <edvardsen.rasmus@gmail.com>
2018-03-30 18:21:28 +0200

    ajax

--- wasteless/Scripts/main.js
+++ wasteless/Scripts/main.js
@@ -9,4 +9,23 @@
             $('#actionsbar').fadeIn();
         });
     });
+    $('#searchbutton').on('click', function () {
+
+        //pre ajax
+        document.body.style.cursor = 'wait';
+        $(this).prop('disabled', true);
+
+        //data for get
+        var $query = $('#querytosearch').val();
+        var $options = $('#queryoptions').val();
+
+        $.ajax({
+            url: "/admin/get/?query=" + $query + "&options=" + $options + "/"
+        }).done(function (data) {
+            console.log(data)
+            //document.body.style.cursor = 'wait';
+        });
+
+
+    });
 });
